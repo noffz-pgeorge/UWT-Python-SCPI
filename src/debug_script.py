7d10ef62d62060a77eec7392b95a7332dde765e7
Peter George <peter.george@noffz.com>
2026-09-14 11:21:54 -0500

    adding rfsg

--- src/debug_script.py
+++ src/debug_script.py
@@ -1,19 +1,26 @@
 from bt_client import BTClient
 from wifi_client import WifiClient
+from rfsg_client import RfsgClient
 
 ip_addr = "169.254.54.113"
 rf_port = 1
 bt = None
 wifi = None
+rfsg = None
 try:
     bt = BTClient(ip_addr, rf_port)
     wifi = WifiClient(ip_addr, rf_port)
+    rfsg = RfsgClient(ip_addr, rf_port)
     bt.open()
     bt.reset()
     wifi.open()
     wifi.reset()
+    rfsg.open()
+    rfsg.reset()
     print(f"bluetooth state is {bt.get_measurement_state()}")
     print(f"wifi state is {wifi.get_measurement_state()}")
+    print(f"rfsg is {rfsg.is_generating()}")
+    print(f"rfsg waveforms {rfsg.get_waveforms_folder()}")
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
@@ -21,3 +28,5 @@ finally:
         bt.disconnect()
     if wifi is not None:
         wifi.disconnect()
+    if rfsg is not None:
+        rfsg.disconnect()
